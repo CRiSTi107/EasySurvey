5bcbf645bfc71d9e9bcd1d730de4886bce0b2fd0
Voiculescu Cristian Constantin <cvoiculescu@icloud.com>
2018-09-29 18:40:35 +0300

    Added MaterialSkin UI for .NET4.0

--- EasySurvey/frm_Main.Designer.cs
+++ EasySurvey/frm_Main.Designer.cs
@@ -28,9 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frm_Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(494, 338);
+            this.Name = "frm_Main";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.frm_Main_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
--- EasySurvey/frm_Main.cs
+++ EasySurvey/frm_Main.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -9,11 +11,21 @@ using System.Windows.Forms;
 
 namespace EasySurvey
 {
-    public partial class frm_Main : Form
+    public partial class frm_Main : MaterialForm
     {
         public frm_Main()
         {
             InitializeComponent();
+
+            var materialSkinManager = MaterialSkinManager.Instance;
+            materialSkinManager.AddFormToManage(this);
+            materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+        }
+
+        private void frm_Main_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
