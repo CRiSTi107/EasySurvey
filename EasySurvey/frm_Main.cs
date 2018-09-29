4c62b6ac14769a3cd04ea1cd39e19501a2548b73
Voiculescu Cristian Constantin <cvoiculescu@icloud.com>
2018-09-29 22:01:24 +0300

    SQLite added to solution

--- EasySurvey/frm_Main.cs
+++ EasySurvey/frm_Main.cs
@@ -9,6 +9,8 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
+using System.Data.SQLite;
+
 namespace EasySurvey
 {
     public partial class frm_Main : MaterialForm
@@ -20,12 +22,32 @@ namespace EasySurvey
             var materialSkinManager = MaterialSkinManager.Instance;
             materialSkinManager.AddFormToManage(this);
             materialSkinManager.Theme = MaterialSkinManager.Themes.LIGHT;
-            materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+            //materialSkinManager.ColorScheme = new ColorScheme(Primary.BlueGrey800, Primary.BlueGrey900, Primary.BlueGrey500, Accent.LightBlue200, TextShade.WHITE);
+            // BLUE
+            materialSkinManager.ColorScheme = new ColorScheme(Primary.Blue800, Primary.Blue900, Primary.Blue500, Accent.LightBlue200, TextShade.WHITE);
+
+            // GREEN
+            //materialSkinManager.ColorScheme = new ColorScheme(Primary.Green800, Primary.Green900, Primary.Green500, Accent.LightGreen200, TextShade.WHITE);
         }
 
         private void frm_Main_Load(object sender, EventArgs e)
         {
+            try
+            {
+                using (SQLiteConnection conn = new SQLiteConnection(@"Data Source=database.db;Version=3;"))
+                using (SQLiteCommand cmd = new SQLiteCommand("SELECT * FROM Question", conn))
+                {
+                    conn.Open();
 
+                    SQLiteDataReader dr = cmd.ExecuteReader();
+
+                    MessageBox.Show("Connected successfully!");
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }
---
+++ sqlite-netFx-source-1.0.109.0/SQLite.Interop/src/generic/interop.h
@@ -0,0 +1,18 @@
+/*
+ * interop.h -
+ *
+ * Written by Joe Mistachkin.
+ * Released to the public domain, use at your own risk!
+ */
+
+#ifndef INTEROP_VERSION
+#define INTEROP_VERSION          "1.0.109.0"
+#endif
+
+#ifndef INTEROP_SOURCE_ID
+#define INTEROP_SOURCE_ID        "86636b58e29f85d17a2194bfacdb3b989a81b2bb"
+#endif
+
+#ifndef INTEROP_SOURCE_TIMESTAMP
+#define INTEROP_SOURCE_TIMESTAMP "2018-08-11 19:58:48 UTC"
+#endif
