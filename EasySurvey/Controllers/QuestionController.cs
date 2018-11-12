e42e45fdbfe2533dcb84dc061933f2f81fddde5e
Voiculescu Cristian Constantin <cvoiculescu@icloud.com>
2018-11-12 17:20:51 +0200

    Added GetQuestions Method in QuestionController

--- EasySurvey/Controllers/QuestionController.cs
+++ EasySurvey/Controllers/QuestionController.cs
@@ -10,6 +10,15 @@ namespace EasySurvey.Controllers
     {
         private DatabaseEntity DatabaseModel = new DatabaseEntity();
 
+        public List<Question> GetQuestions(long SurveyID)
+        {
+            List<Question> Questions;
+            Questions = new List<Question>(from surveyDefinition in DatabaseModel.SurveyDefinition
+                                           join question in DatabaseModel.Question on surveyDefinition.QuestionID equals question.QuestionID
+                                           where surveyDefinition.SurveyID == SurveyID
+                                           select question);
 
+            return Questions;
+        }
     }
 }
