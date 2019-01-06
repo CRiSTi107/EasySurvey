b1f52af6f68d6470398b6978b844011357f78dfa
Voiculescu Cristian Constantin <cvoiculescu@icloud.com>
2019-01-06 16:57:53 +0200

    Installer Project Created

---
+++ Installer/MainMenu.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace Installer
+{
+    partial class MainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainMenu));
+            this.btn_Back = new System.Windows.Forms.Button();
+            this.btn_Next = new System.Windows.Forms.Button();
+            this.btn_Cancel = new System.Windows.Forms.Button();
+            this.grb_MenuButtons = new System.Windows.Forms.GroupBox();
+            this.grb_MenuButtons.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btn_Back
+            // 
+            this.btn_Back.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Back.Location = new System.Drawing.Point(199, 19);
+            this.btn_Back.Name = "btn_Back";
+            this.btn_Back.Size = new System.Drawing.Size(75, 23);
+            this.btn_Back.TabIndex = 0;
+            this.btn_Back.Text = "< &Back";
+            this.btn_Back.UseVisualStyleBackColor = true;
+            this.btn_Back.Click += new System.EventHandler(this.btn_Back_Click);
+            // 
+            // btn_Next
+            // 
+            this.btn_Next.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Next.Location = new System.Drawing.Point(280, 19);
+            this.btn_Next.Name = "btn_Next";
+            this.btn_Next.Size = new System.Drawing.Size(75, 23);
+            this.btn_Next.TabIndex = 1;
+            this.btn_Next.Text = "&Next >";
+            this.btn_Next.UseVisualStyleBackColor = true;
+            this.btn_Next.Click += new System.EventHandler(this.btn_Next_Click);
+            // 
+            // btn_Cancel
+            // 
+            this.btn_Cancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btn_Cancel.Location = new System.Drawing.Point(397, 19);
+            this.btn_Cancel.Name = "btn_Cancel";
+            this.btn_Cancel.Size = new System.Drawing.Size(75, 23);
+            this.btn_Cancel.TabIndex = 2;
+            this.btn_Cancel.Text = "&Cancel";
+            this.btn_Cancel.UseVisualStyleBackColor = true;
+            this.btn_Cancel.Click += new System.EventHandler(this.btn_Cancel_Click);
+            // 
+            // grb_MenuButtons
+            // 
+            this.grb_MenuButtons.Controls.Add(this.btn_Cancel);
+            this.grb_MenuButtons.Controls.Add(this.btn_Next);
+            this.grb_MenuButtons.Controls.Add(this.btn_Back);
+            this.grb_MenuButtons.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.grb_MenuButtons.Location = new System.Drawing.Point(0, 406);
+            this.grb_MenuButtons.Name = "grb_MenuButtons";
+            this.grb_MenuButtons.Size = new System.Drawing.Size(484, 56);
+            this.grb_MenuButtons.TabIndex = 3;
+            this.grb_MenuButtons.TabStop = false;
+            // 
+            // MainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(484, 462);
+            this.Controls.Add(this.grb_MenuButtons);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(500, 500);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "MainMenu";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Easy Survey - Installer";
+            this.Load += new System.EventHandler(this.MainMenu_Load);
+            this.grb_MenuButtons.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Back;
+        private System.Windows.Forms.Button btn_Next;
+        private System.Windows.Forms.Button btn_Cancel;
+        private System.Windows.Forms.GroupBox grb_MenuButtons;
+    }
+}
+
---
+++ Installer/MainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Installer
+{
+    public partial class MainMenu : Form
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void MainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Back_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Next_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_Cancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
